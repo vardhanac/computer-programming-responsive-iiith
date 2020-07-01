8ce03f3d83ec2592bd625ca3d0e29cb32c1b503f
vardhanac <67281786+vardhanac@users.noreply.github.com>
2020-07-01 22:51:09 +0530

    Html file added

--- src/lab/exp4/simulation/2-D/array.js
+++ src/lab/exp4/simulation/2-D/array.js
@@ -121,7 +121,8 @@ window.view = {
 		var size = this.rowsA * this.colsA
 		for ( i = 0 ; i < size ; i++)
 		{
-			var random = Math.floor(Math.random()*15)
+			var data = i+1
+			var random = Math.floor(data)
 			this.matrixA.push(random)
 		}
 		this.resetRowsAndCols()
@@ -137,7 +138,8 @@ window.view = {
 		var size = this.rowsB * this.colsB
 		for ( i = 0 ; i < size ; i++)
 		{
-			var random = Math.floor(Math.random()*15)
+			var data = i+1
+			var random = Math.floor(data)
 			this.matrixB.push(random)
 		}
 		this.disableButton('generateB')
